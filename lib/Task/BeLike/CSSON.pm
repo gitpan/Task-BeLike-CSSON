bbf43f368fbe7f11e79008e8f69199937d58e30f
Erik Carlsson <info@code301.com>
2014-11-29 16:07:50 +0000

    Import of CSSON/Task-BeLike-CSSON-0.0001.tar.gz from CPAN.
    gitpan-cpan-distribution: Task-BeLike-CSSON
    gitpan-cpan-version:      0.0001
    gitpan-cpan-path:         CSSON/Task-BeLike-CSSON-0.0001.tar.gz
    gitpan-cpan-author:       CSSON
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/CSSON.pm
@@ -0,0 +1,35 @@
+package Task::BeLike::CSSON;
+$Task::BeLike::CSSON::VERSION = '0.0001';
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::BeLike::CSSON - Stuff I need
+
+=head1 SYNOPSIS
+
+  $ cpanm Task::BeLike::CSSON
+
+=head1 TASK CONTENTS
+
+See cpanfile.
+
+=head1 AUTHOR
+
+Erik Carlsson E<lt>info@code301.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Erik Carlsson
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
